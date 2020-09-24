71f8a6b584fbe0ef5a3b03c01b01e26225d850de
Erdenebold <ebo2020@stanford.edu>
2020-09-24 13:58:18 -0700

    Tests passed

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,4 +1,5 @@
 #include "byte_stream.hh"
+#include <iostream>
 
 // Dummy implementation of a flow-controlled in-memory byte stream.
 
@@ -6,48 +7,71 @@
 // automated checks run by `make check_lab0`.
 
 // You will need to add private members to the class declaration in `byte_stream.hh`
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+//Init private members
+ByteStream::ByteStream(const size_t capacity): cap(capacity), deq() {
+}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+	//size_t size_left = capacity - deq.size();
+	//If data is empty, signal input ended
+	if (data.size() == 0){
+		_end = true;
+	}
+	//Data can be bigger than size_left
+	size_t size = min(data.size(), remaining_capacity());
+	bytes_wrote += size;
+	for (size_t i=0; i<size; i++){
+		deq.push_back(data[i]);
+	}
+	return size;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    string str_peeked;
+    size_t size = min(len, deq.size());
+    for (size_t i=0; i<size; i++){
+    	str_peeked += deq[i];
+    }
+    //bytes_readed += size;
+    return str_peeked;
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) {
+	size_t size = min(len, deq.size());
+       for(size_t i=0; i<size; i++){
+       //Bytes sent first are at the front
+       deq.pop_front();
+       }
+	bytes_readed += size;       
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+	//Read
+    	string str_readed = peek_output(len);
+	//Pop
+	pop_output(len);
+    	return str_readed;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() {_end = true; }
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return _end; }
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const{ return deq.size(); }
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const { return deq.size() == 0; }
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const { return buffer_empty() && input_ended(); }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return bytes_wrote; }
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return bytes_readed; }
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const { return cap - deq.size(); }
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -2,7 +2,7 @@
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
 #include <string>
-
+#include <deque>
 //! \brief An in-order byte stream.
 
 //! Bytes are written on the "input" side and read from the "output"
@@ -16,9 +16,12 @@ class ByteStream {
     // all, but if any of your tests are taking longer than a second,
     // that's a sign that you probably want to keep exploring
     // different approaches.
-
+    bool _end{}; // Flag for ending stream at will 
     bool _error{};  //!< Flag indicating that the stream suffered an error.
-
+    size_t cap;
+    size_t bytes_wrote = 0; 
+    size_t bytes_readed = 0;  
+    std::deque<char> deq;
   public:
     //! Construct a stream with room for `capacity` bytes.
     ByteStream(const size_t capacity);
@@ -35,6 +38,7 @@ class ByteStream {
     size_t remaining_capacity() const;
 
     //! Signal that the byte stream has reached its ending
+    //Modified this line
     void end_input();
 
     //! Indicate that the stream suffered an error.
