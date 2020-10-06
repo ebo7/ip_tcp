310b0b73a76891383762fdad95ebbceeb6fe79c3
Erdenebold <ebo2020@stanford.edu>
2020-10-05 20:06:13 -0700

    Debug wrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,5 @@
 #include "wrapping_integers.hh"
-
+#include <math.h>
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,8 +14,10 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+  uint64_t mod = (static_cast<uint64_t>(1)) << 32;
+  return WrappingInt32{
+    static_cast<uint32_t>((n + isn.raw_value()) % mod)
+      };
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +31,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+  uint64_t e31 = static_cast<uint64_t>(1) << 31;
+  uint64_t e32 = static_cast<uint64_t>(1) << 32;
+  uint64_t mid = checkpoint + isn.raw_value();
+  uint64_t num_cycle = mid / e32;
+  if (n.raw_value() < e31){
+    return n.raw_value() + num_cycle * e32 - isn.raw_value();
+  }
+  return n.raw_value() + (num_cycle - 1) * e32 - isn.raw_value();
+  
 }
